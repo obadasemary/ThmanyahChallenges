11c53832d0aa619b4647d8ec9e79d33868ca3265
Abdelrahman Mohamed <obada.semary@gmail.com>
2025-08-15 16:07:07 +0300

    Initial Commit

---
+++ ThmanyahChallenges/ThmanyahChallengesApp.swift
@@ -0,0 +1,17 @@
+//
+//  ThmanyahChallengesApp.swift
+//  ThmanyahChallenges
+//
+//  Created by Abdelrahman Mohamed on 15.08.2025.
+//
+
+import SwiftUI
+
+@main
+struct ThmanyahChallengesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ThmanyahChallengesTests/ThmanyahChallengesTests.swift
@@ -0,0 +1,17 @@
+//
+//  ThmanyahChallengesTests.swift
+//  ThmanyahChallengesTests
+//
+//  Created by Abdelrahman Mohamed on 15.08.2025.
+//
+
+import Testing
+@testable import ThmanyahChallenges
+
+struct ThmanyahChallengesTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
